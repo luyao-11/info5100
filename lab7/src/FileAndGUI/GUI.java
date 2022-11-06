98c45eb4a95964214253236641ed59d002cf2b3e
luyao-11 <109508368+luyao-11@users.noreply.github.com>
2022-11-05 21:47:39 -0700

    Update GUI.java

--- lab7/src/FileAndGUI/GUI.java
+++ lab7/src/FileAndGUI/GUI.java
@@ -3,10 +3,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
 
-class GUI extends JFrame {
-    public static void placeComponents(JPanel panel) {
+
+public class GUI {
+    String[] show;
+    GUI() {
+        JFrame frame = new JFrame("Read and Write file");
+        frame.setBounds(300, 200, 900, 500);
+        //create and add panel
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         //set basic element in the panel
         JLabel label1 = new JLabel("Reading File Name");
@@ -15,7 +23,7 @@ class GUI extends JFrame {
         JLabel label2 = new JLabel("First five lines of file:");
         JTextArea textArea1 = new JTextArea();
 
-        //add element of reading part into panel
+        //add elements of reading part into panel and set reading part layout 
         panel.setLayout(null);
         panel.add(label1);
         label1.setForeground(Color.blue);
@@ -30,26 +38,29 @@ class GUI extends JFrame {
         panel.add(textArea1);
         textArea1.setBounds(20, 180, 350, 200);
 
+        //show the content of read method 
         button1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textArea1.setText("");
-                ReadAndWriteFile fileRead = new ReadAndWriteFile();
-                String path = textField1.getText();
-
-                ArrayList content = fileRead.readFile(path);
-                textArea1.setText(content.get(0)+"\n"+content.get(1)+"\n"+content.get(2)+"\n"+content.get(3)+"\n"+content.get(4)
-                );
+                String inputFileName = textField1.getText();
+                ReadAndWriteFile read = new ReadAndWriteFile();
+                try {
+                    show = read.readFile(inputFileName);
+                    textArea1.setText(show[0]);
+                } catch (Exception c) {
+                    c.printStackTrace();
+                }
             }
         });
 
-        ////add element of writing part into panel
+        //add element of writing part into panel
         JLabel label3 = new JLabel("File name to write ");
         JTextField textField2 = new JTextField("<user input>");
         JButton button2 = new JButton("Click to write to file");
         JLabel label4 = new JLabel("First five lines of file");
         JTextArea textArea2 = new JTextArea();
-
+        
+        //and element of writing part into panel and set writing part layout 
         panel.add(label3);
         label3.setForeground(Color.blue);
         label3.setBounds(500, 20, 350, 30);
@@ -62,16 +73,16 @@ class GUI extends JFrame {
         label4.setBounds(500, 150, 350, 30);
         panel.add(textArea2);
         textArea2.setBounds(500, 180, 350, 200);
-
-
-        button1.addActionListener(new ActionListener() {
+        
+        //write out three field of first five line
+        button2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                ReadAndWriteFile writeFile = new ReadAndWriteFile();
+                textArea2.setText(show[1]);
+                writeFile.write(textField2.getText(), show[1]);
             }
-
         });
-
-
     }
 }
 
