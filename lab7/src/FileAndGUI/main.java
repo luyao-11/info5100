f8ac0e15e59206b37ac80df3e8a6d2e5b78f4777
luyao-11 <109508368+luyao-11@users.noreply.github.com>
2022-11-05 21:43:35 -0700

    Update main.java

--- lab7/src/FileAndGUI/main.java
+++ lab7/src/FileAndGUI/main.java
@@ -4,13 +4,6 @@ import javax.swing.*;
 
 public class main {
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Read and Write file");
-        frame.setBounds(300, 200, 900, 500);
-        //create and add panel
-        JPanel panel = new JPanel();
-        frame.add(panel);
-        FileAndGUI.GUI.placeComponents(panel);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    new GUI();
     }
 }
